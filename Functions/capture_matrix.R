c46d91c720b2ddbfeb38cb1cffcaeca1798a4d2f
JohnRFoster <fosterj@bu.edu>
2021-10-07 16:36:47 -0400

    start making wide

--- Functions/capture_matrix.R
+++ Functions/capture_matrix.R
@@ -4,33 +4,40 @@
 #' 1 = alive
 #' 2 = dead
 #' 
-#' @param unit the unit to extract; plot, site, domain
-#' @param data the small mammal csv in /Data
+#' @param unit the unit to extract; plot ("HARV_016"), site ("HARV"), domain ("D02")
+#' @param neon.smam the small mammal neon.smam frame from the csv in /neon.smam
 #' @param by_bout should capture history be for each collectDate (the default) or by trapping bout?
 
 
-capture_matrix <- function(unit, data, by_bout = FALSE){
+capture_matrix <- function(unit, neon.smam, by_bout = FALSE){
+  
+  if(!exists("neon.smam")) {
+    neon.smam <- read_csv("Data/allSmallMammals.csv")
+  }
   
   if(grepl("_[[:digit:]]{3}", unit)){ # plot
     scale <- "plot"
-    df <- data %>% 
+    df <- neon.smam %>% 
       filter(plotID == unit)
   } else if(grepl("[[:alpha:]]{4}", unit)){ # site
     scale <- "site"
-    df <- data %>% 
+    df <- neon.smam %>% 
       filter(siteID == unit)
   } else if(grepl("D[[:digit:]]{2}", unit)){ # domain
     scale <- "domin"
-    df <- data %>% 
+    df <- neon.smam %>% 
       filter(domainID == unit)
   } else {
     stop("unit argument not recognized")
   }
+  message("Scale = ", scale)
+  
   
   # control flow for by bout
-  
   ch <- df %>% 
-    select(tagID, collectDate)
+    select(tagID, collectDate, animalInTrap) %>% 
+    pivot_wider(names_from = collectDate,
+                values_from = animalInTrap)
     
   
 }
