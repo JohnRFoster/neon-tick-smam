5a2d8267ca1dda23a10a3e4b6e611853b31d2828
John Foster <johnfoster@dhcp-wifi-8021x-168-122-212-63.bu.edu>
2021-10-19 11:15:09 -0400

    site and Peromyscus only, some tests

--- Functions/capture_matrix.R
+++ Functions/capture_matrix.R
@@ -4,86 +4,104 @@
 #' 1 = alive
 #' 2 = dead
 #' 
-#' @param unit the unit to extract; plot ("HARV_016"), site ("HARV"), domain ("D02")
+#' @param site the unit to extract; plot ("HARV_016"), site ("HARV"), domain ("D02")
 #' @param neon.smam the small mammal neon.smam frame from the csv in /neon.smam
 #' @param by_bout should capture history be for each collectDate (the default) or by trapping bout?
 
 
-capture_matrix <- function(unit, neon.smam, by_bout = FALSE){
+capture_matrix <- function(site, neon.smam, by_bout = FALSE){
   
-  if(!exists("neon.smam")) {
-    neon.smam <- read_csv("Data/allSmallMammals.csv")
+  neon.smam <- neon.smam %>% 
+    filter(siteID == site)
+  
+  all.days <- neon.smam %>% pull(collectDate) %>% unique()
+  
+  df <- neon.smam %>%
+    filter(genusName == "Peromyscus") 
+  
+  mice.days <- df %>% pull(collectDate) %>% unique()
+  
+  # if Peromyscus was not seen on all days, need to fill days back in
+  fill.days <- tibble()
+  if(length(mice.days) < length(all.days)){
+    missing.days <- all.days[which(!(all.days %in% mice.days))]
+    fill.days <- neon.smam %>% 
+      filter(collectDate %in% missing.days) %>% 
+      distinct(collectDate, .keep_all = TRUE) %>% 
+      select(collectDate, tagID) %>% 
+      mutate(tagID = "noCapture",
+             state = 4)
   }
   
-  if(grepl("_[[:digit:]]{3}", unit)){ # plot
-    scale <- "plot"
-    df <- neon.smam %>% 
-      filter(plotID == unit)
-  } else if(grepl("[[:alpha:]]{4}", unit)){ # site
-    scale <- "site"
-    df <- neon.smam %>% 
-      filter(siteID == unit)
-  } else if(grepl("D[[:digit:]]{2}", unit)){ # domain
-    scale <- "domin"
-    df <- neon.smam %>% 
-      filter(domainID == unit)
-  } else {
-    stop("unit argument not recognized")
-  }
-  message("Scale = ", scale)
+  # the states that we need are:
+  # 1 = dead
+  # 2 = unobserved
+  # 3 = observed mouse with tick attached
+  # 4 = observed mouse without tick attached
+  # 5 = observed mouse with unknown tick status
+
+  # the total number of unique tags
+  total.ind <- df %>% filter(!is.na(tagID)) %>% 
+    pull(tagID) %>% 
+    unique() %>% 
+    length()
   
+  # get the state of each mouse at each trap night
+  df.state <- df %>% 
+    mutate(tickOn = if_else(adultTicksAttached == "Y" |                # any life stage observed
+                              nymphalTicksAttached == "Y" |
+                              larvalTicksAttached == "Y", 3, 0),
+           tickOn = if_else(adultTicksAttached == "N" &                # all life stages not observed
+                              nymphalTicksAttached == "N" &
+                              larvalTicksAttached == "N", 4, tickOn),
+           tickOn = if_else(adultTicksAttached == "U" &                # all life stages unknown
+                              nymphalTicksAttached == "U" &
+                              larvalTicksAttached == "U", 5, tickOn),
+           tickOn = if_else(is.na(tickOn), 3, tickOn),                 # NAs get unknown status
+           state = if_else(animalInTrap == 1 & tickOn == 1, 3, 0),     # observed animal with tick attached
+           state = if_else(animalInTrap == 1 & tickOn == 0, 4, state), # observed animal without tick attached
+           state = if_else(animalInTrap == 1 & tickOn == 3, 5, state), # observed animal with unknown tick status
+           state = if_else(animalInTrap == 0, 2, state),               # unobserved
+           state = if_else(animalInTrap == 2, 1, state),               # dead 
+           tagID = if_else(is.na(tagID) & animalInTrap == 0, 
+                           "noCapture", tagID)) %>%     # placeholder for trap nights without any captures           
+    select(tagID, collectDate, state) %>% 
+    filter(!is.na(tagID)) 
   
-  # control flow for by bout
+  # add missing days
+  df.all.days <- bind_rows(df.state, fill.days) 
   
-  # capture history with plot level identifiers
-  ch.by.plot <- df %>% 
-    # select(tagID, collectDate, animalInTrap, plotID) %>% 
-    mutate(plotNumber = as.numeric(as.factor(plotID)), # numeric plot id
-           plotCaptureStatus = plotNumber + animalInTrap - 1, # if seen, plot number
-           plotCaptureStatus = if_else(animalInTrap == 0, 0, plotCaptureStatus), # revert not-seen to 0
-           plotCaptureStatus = if_else(animalInTrap == 2, -1, plotCaptureStatus), # dead animals to -1
-           tagID = if_else(is.na(tagID) & animalInTrap == 0,
-                           "noCapture", # dates without captures, need for full matrix
-                           tagID)) %>% 
+  ch <- df.all.days %>% 
     arrange(collectDate) %>% 
-    filter(!is.na(tagID)) # some tags are just NA - remove
-  
-  # need to figure out what to do about daily capture inconsistencies across plots 
-  ch <- ch.by.plot %>% 
-    select(tagID, collectDate, plotCaptureStatus) %>% 
-    distinct() %>% 
+    # group_by(collectDate, tagID) %>%
+    # distinct() %>% 
+    # ungroup() %>%
     pivot_wider(names_from = collectDate,
-                # values_fn = length,
-                values_from = plotCaptureStatus,
-                values_fill = 0) %>% 
+                values_from = state,
+                values_fill = 4, # unobserved state
+                values_fn = {max} # conflicting states get unknown designation 
+                ) %>%
     filter(tagID != "noCapture")
   
-  # make sure mice that are recorded dead stay dead!
-  for(i in 1:nrow(ch)){
-    ind <- ch[i,]
-    if(any(ind == 2)){
-      tod <- which(ind == 2)
-      if(sum(ind[(tod+1):length(ind)]) > 0){
-        stop("Zombie mouse!")
-      }
+  # check dimensions are what they should be
+  if(ncol(ch[,-1]) != length(all.days)) stop("Conflicting days in data vs capture matrix")
+  if(nrow(ch) != total.ind) stop("Conflicting individuals in data vs capture matrix")
+  
+  # make sure mice that are recorded dead stay dead! 
+  mice.found.dead <- which(apply(ch[,-1], 1, function(x) any(x == 1))) # mice that died
+  
+  # all days after found dead should have state of 4
+  check.dead <- function(x){
+    day.dead <- which(x == 1)
+    if(all(x[(day.dead+1):length(x)] == 4)){
+      return(1)
+    } else {
+      return(0)
     }
   }
   
-  # get animal identifiers    
-  smam.ind <- pull(ch, tagID)
-  ids <- df %>% 
-    filter(tagID %in% smam.ind) %>% 
-    select(tagID, genusName, speciesName, nlcdClass, siteID, plotID) %>% 
-    distinct() 
+  dead.test <- apply(ch[mice.found.dead,-1], 1, check.dead)
+  if(all(dead.test != 1)) stop("Zombie mouse!")
   
-  # if animals are identified differently after first capture
-  # or found in more than one plot
-  if(nrow(ids) != nrow(ch)){
-    
-  }
-  
-  # might want to do plots by time and individual?
-    
-  
-  
+  return(ch)
 }
