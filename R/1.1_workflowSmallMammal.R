867bcb34776bd0fecba336f2969a89324c67a162
JohnRFoster <fosterj@bu.edu>
2021-10-25 14:44:38 -0400

    array job for individual chains

--- R/1.1_workflowSmallMammal.R
+++ R/1.1_workflowSmallMammal.R
@@ -1,4 +1,6 @@
-# mouse workflow 
+# ========================================================== #
+#       Workflow script for multi-state mouse models         #
+# ========================================================== #
 
 library(tidyverse)
 
@@ -9,11 +11,12 @@ out.dir <- "/projectnb/dietzelab/fosterj/FinalOut/neon-tick-smam/mice"
 model.dir <- "multiStateBasic"
 
 # script flow
-production <- FALSE
+production <- TRUE
 run.intake <- FALSE
 check.neon <- FALSE
 run.parallel <- FALSE
 array.job <- FALSE
+n.chains <- 4
 
 # sites to run
 site.coord <- readr::read_csv("Data/siteLatLon.csv")
@@ -22,17 +25,25 @@ sites <- site.coord %>% pull(siteID)
 # jags arguments
 if(production) {
   n.adapt <- 5000
-  n.chains <- 1
   thin <- 1
   n.iter <- 100000
   n.loops <- 1000
-} else {
+  
+  jobs <- tibble(site = rep(sites, each = n.chains),
+                 chain = rep(1:n.chains, length(sites)))
+  
+  # run the model individually at each site
+  array.num <- as.numeric(Sys.getenv("SGE_TASK_ID")) # read array job number
+  site <- jobs %>% slice(array.num) %>% pull(site)
+  chain <- jobs %>% slice(array.num) %>% pull(chain)
+  
+} else { # testing / dev
   n.adapt <- 50
-  n.chains <- 1
   thin <- 1
   n.iter <- 50
   n.loops <- 10
-  sites <- sites[sample(length(sites), 1)]
+  site <- sites[sample(length(sites), 1)]
+  chain <- 1
 }
 
 
@@ -44,23 +55,18 @@ if(run.intake){
   message(paste0("Mouse intake complete at ", Sys.time()))
 }
 
-# run the model individually at each site
-for(s in seq_along(sites)){
-  site <- sites[s]
+# create file path for output
+site.dir <- file.path(out.dir, model.dir, site)
+if(!dir.exists(site.dir)) dir.create(site.dir, recursive = TRUE)
   
-  site.dir <- file.path(out.dir, model.dir, site)
+start.time <- Sys.time()
+message(paste("Running mouse model at", site, start.time))
+message(paste0("Chain: ", chain))
   
-  # create file path for output
-  if(!dir.exists(site.dir)) dir.create(site.dir, recursive = TRUE)
+source("R/4.1_setup_smallMammal.R")
   
-  start.time <- Sys.time()
-  message(paste("Running mouse model at", site, start.time))
-  
-  source("R/4.1_setup_smallMammal.R")
-  
-  end.time <- Sys.time()
-  message(paste("  ", site, "completed at", end.time))
-  message(paste("   Total run time:", end.time-start.time))
-}
+end.time <- Sys.time()
+message("Total run time:")
+print(end.time-start.time)
 
 
