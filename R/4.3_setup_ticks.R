acd0738e1ff163e8c3582ae386fff6753827eae2
JohnRFoster <fosterj@bu.edu>
2022-01-04 16:02:04 -0500

    getting cumGDD in models, correct plot indexing

--- R/4.3_setup_ticks.R
+++ R/4.3_setup_ticks.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(lubridate)
 
-site <- "TREE"
+site <- "UKFS"
 
 source("Functions/tick_data.R")
 tick.ls <- tick_data(site)
@@ -14,48 +14,65 @@ if(constants$n.species == 1){
 } else {
   Y.init <- constants$Y.init
 }
- 
-daily.ground.temp <- read_csv("Data/bioTempDaily.csv")
-met.df <- daily.ground.temp %>% 
-  filter(siteID == site) %>% 
-  arrange(Date) %>% 
-  group_by(Year) %>% 
-  mutate(growingDegree = if_else(bioTempMaximum > 0, bioTempMaximum, 0)) %>% 
-  mutate(cumGDD = cumsum(growingDegree))
+## TO-DO
+# add missing dates into met data to get NA cumulative gdd values
+# index met with tick observations
 
-gdd <- rep(seq(0, 3000, length.out = 365), 6)
-constants$gdd <- gdd[1:constants$N]
+# need dates starting with Jan. 1 of the first year through last observation date
+days.year <- seq.Date(ymd(paste0(min(year(constants$all.days)), "-01-01")), 
+                      max(constants$all.days),
+                      by = 1)
+
+days.df <- tibble(Date = days.year)
+daily.air.temp <- read_csv("Data/airTempDaily.csv")
+daily.air.temp <- daily.air.temp %>% filter(siteID == site)
+
+met.df <- left_join(days.df, daily.air.temp, by = "Date")
+
+met.df <- met.df %>% 
+  arrange(Date) %>% 
+  group_by(Year) %>%
+  mutate(growingDegree = if_else(tempTripleMaximum > 0, tempTripleMaximum, 0),
+         cumGDD = cumsum(growingDegree)) %>% 
+  filter(Date %in% constants$all.days)
+
+
+cumGDD <- pull(met.df, cumGDD)
+cumGDD.var <- pull(met.df, airTempMaximumVariance)
+data$gdd.mu <- cumGDD
+data$gdd.mu[is.na(cumGDD)] <- approx(1:length(cumGDD), cumGDD, xout = which(is.na(cumGDD)))$y
+
+data$gdd.tau <- 1 / cumGDD.var
+data$gdd.tau[is.na(cumGDD.var)] <- mean(data$gdd.tau, na.rm = TRUE)
+
+constants$gdd.max <- max(cumGDD, na.rm = T) + 10
 constants$tau.dem <- 0.04
 constants$ns <- 4
 
 data$x.init <- rep(10, 4)
 
-#### TO DO
-# TRY WITH A INDEXED BY PLOT OR PLOT SPECIFIC INDICIES TO MATCH DAYS WHEN PERMUTING A MATRIX
-      # WILL NEED THE A LATTER WHEN ADDING WEATHER
-  
-inits <- function(){list(px = 0.1 + jitter(Y.init[,-1,]),
-                         x = 1 + round(jitter(Y.init)),
-                            repro.mu = jitter(20),
-                            phi.l.mu = jitter(0),
-                            phi.n.mu = jitter(0),
-                            phi.a.mu = jitter(0),
-                            theta.ln = jitter(0),
-                            theta.na = jitter(0),
-                            tau.obs.l = jitter(2),
-                            tau.obs.n = jitter(2),
-                            tau.obs.a = jitter(2),
-                            tau.l = jitter(2),
-                            tau.d = jitter(2),
-                            tau.n = jitter(2),
-                            tau.a = jitter(2))}
+inits <- function() {
+  list(
+    px = 0.1 + abs(jitter(Y.init[, -1, ])),
+    x = 1 + abs(jitter(Y.init)),
+    gdd = abs(jitter(data$gdd.mu)),
+    repro.mu = jitter(20),
+    phi.l.mu = jitter(3),
+    phi.n.mu = jitter(4),
+    phi.a.mu = jitter(5),
+    theta.ln = jitter(0),
+    theta.na = jitter(0),
+    tau.obs = abs(rnorm(3, 10, 5)),
+    sig = abs(rnorm(4, 0, 10))
+  )
+}
 
 message("Build model")
 source("R/3.1_tickStaticOneSpecies.R")
 model <- nimbleModel(model.code,
-                          constants = constants,
-                          data = data,
-                          inits = inits())
+                     constants = constants,
+                     data = data,
+                     inits = inits())
 
 model$initializeInfo()
 cModel <- compileNimble(model)
