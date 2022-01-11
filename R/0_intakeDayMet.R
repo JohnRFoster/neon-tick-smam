997c7c21e8d45556ebbd43c61258f38debd58ed6
JohnRFoster <fosterj@bu.edu>
2022-01-11 11:38:11 -0500

    leap year fix

--- R/0_intakeDayMet.R
+++ R/0_intakeDayMet.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(daymetr)
+library(curl)
 
 
 site.coord <- readr::read_csv("Data/siteLatLon.csv") %>% suppressMessages()
@@ -24,24 +25,74 @@ plot.df <- plot.df %>%
   rename_with( ~ c("site", "latitude", "longitude"), everything())
 write_csv(plot.df, path = "Data/plotLatLon.csv")
 
-s <- 1
-dm <- download_daymet(
-  site = plot.df$site[s],
-  lat = plot.df$latitude[s],
-  lon = plot.df$longitude[s],
-  start = 2014,
-  end = 2020,
-  internal = TRUE,
-  silent = FALSE,
-  force = FALSE,
-  simplify = TRUE
-)
-# 
 # dm <- download_daymet_batch(
 #   file_location = 'Data/plotLatLon.csv',
 #   start = 2014,
 #   end = 2021,
 #   internal = TRUE
 # )
+# 
+# write_csv(dm, path = "Data/daymet.csv")
 
-write_csv(dm, path = "Data/daymet.csv")
+
+data <- read_csv("Data/daymet.csv")
+df <- data %>% 
+  mutate(Date = as.Date(yday-1, 
+                        origin = paste0(year, "-01-01")))
+
+variables <- c(
+  "dayl..s.",      # day length
+  "prcp..mm.day.", # precipitation
+  "srad..W.m.2.",  # shortwave radiation
+  "swe..kg.m.2.",  # snow-water equivalent
+  "tmax..deg.c.",  # maximum temperature
+  "tmin..deg.c.",  # minimum temperature
+  "vp..Pa."        # vapor pressure
+)
+
+variable.name <- c(
+  "dayLength",
+  "precipitation",
+  "shortwaveRadiation",
+  "snowWaterEquivalent",
+  "maxTemperature",
+  "minTemperature",
+  "vaporPressure"
+)
+
+for(s in seq_along(variables)){
+  df.save <- df %>% 
+    filter(measurement == variables[s]) %>% 
+    select(-measurement) 
+  
+  for(i in seq_along(unique(df.save$site))){
+    leap.df <- df.save %>% 
+      filter(site == unique(df.save$site)[i], 
+             Date == "2016-12-30" | Date == "2017-01-01")
+    
+    leap.value <- leap.df %>% 
+      summarise(value = mean(value)) %>% 
+      pull(value)
+      
+    leap.df <- leap.df %>%   
+      add_row(value = leap.value,
+              Date = ymd("2016-12-31"),
+              site = leap.df$site[1],
+              tile = leap.df$tile[1],
+              latitude = leap.df$latitude[1],
+              longitude = leap.df$longitude[1],
+              altitude = leap.df$altitude[1],
+              year = leap.df$year[1],
+              yday = 366) %>% 
+      filter(yday == 366)
+    
+    df.save <- bind_rows(df.save, leap.df)
+  }
+  df.save <- df.save %>% 
+    separate(site, c("data", "plotID"), sep = 4) %>%
+    filter(plotID != "ORNL_006") %>% 
+    rename_at(vars(value), ~ variable.name[s])  %>% 
+    arrange(plotID, Date)
+  
+  write_csv(df.save, path = file.path("Data", paste0("daymet_", variable.name[s], ".csv")))
+}
