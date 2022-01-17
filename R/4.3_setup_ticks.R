4b47ee67a2d581fb3a7dffc87346f6c1f7e28502
JohnRFoster <fosterj@bu.edu>
2022-01-17 16:20:19 -0500

    species setup

--- R/4.3_setup_ticks.R
+++ R/4.3_setup_ticks.R
@@ -1,10 +1,9 @@
 library(tidyverse)
 library(lubridate)
+library(nimble)
 
-site <- "TREE"
-spp <- "Ixodes scapularis"
 source("Functions/tick_data.R")
-tick.ls <- tick_data(site)
+tick.ls <- tick_data(site, species)
 data <- tick.ls$data
 constants <- tick.ls$constants
 
@@ -16,7 +15,7 @@ if(constants$n.species == 1){
 }
 
 # Y.init[2,,] <- Y.init[2,,] + 500
-Y.init <- Y.init + 10
+Y.init <- Y.init / 160
 
 # get cumGDD
 source("Functions/daymet_functions.R")
@@ -40,9 +39,10 @@ data$cumGDD.mu <- all.cumGDD %>%
 
 # transition thresholds
 trans.cgdd <- read_csv("Data/cumgddThresholds.csv") 
+spp.filter <- if_else(species == "IX", "Ixodes scapularis", "Amblyomma americanum")
 trans.cgdd <- trans.cgdd %>% 
   filter(siteID == site,
-         scientificName == spp) 
+         scientificName == spp.filter) 
 data$rho.l1 <- trans.cgdd %>% filter(lifeStage == "Larva") %>% pull(start)
 data$rho.l2 <- trans.cgdd %>% filter(lifeStage == "Larva") %>% pull(end)
 data$rho.n1 <- trans.cgdd %>% filter(lifeStage == "Nymph") %>% pull(start)
@@ -53,7 +53,7 @@ data$rho.a2 <- trans.cgdd %>% filter(lifeStage == "Adult") %>% pull(end)
 constants$tau.dem <- 0.04
 constants$ns <- 4
 
-data$x.init <- rep(10, 4)
+data$x.init <- rep(1, 4)
 
 inits <- function() {
   list(
@@ -66,44 +66,71 @@ inits <- function() {
     phi.a.mu = jitter(5),
     theta.ln = jitter(-6),
     theta.na = jitter(-6),
-    tau.obs = abs(rnorm(3, 10, 5)),
     tau.gdd = abs(rnorm(1, 5, 2)),
-    sig = abs(rnorm(4, c(1e+5, 1000, 1, 20), 3))
+    sig = abs(rnorm(4, c(100, 100, 100, 100), 30))
   )
 }
 itest <- inits()
 
-message("Build model")
-source("R/3.1_tickStaticOneSpecies.R")
-model <- nimbleModel(model.code,
-                     constants = constants,
-                     data = data,
-                     inits = inits())
+if(check.model){
+  message("Build model")
+  model <- nimbleModel(model.code,
+                       constants = constants,
+                       data = data,
+                       inits = inits())
+  
+  model$initializeInfo()
+  cModel <- compileNimble(model)
+  monitor.check <- monitor[monitor != "x"]
+  mcmcConf <- configureMCMC(cModel, monitors = monitor.check)
+  
+  # mcmcConf$removeSampler(target = "phi.a.mu")
+  # mcmcConf$addSampler(target = "phi.a.mu", type = "slice")
+  # mcmcConf$removeSampler(target = "phi.l.mu")
+  # mcmcConf$addSampler(target = "phi.l.mu", type = "slice")
+  # mcmcConf$removeSampler(target = "phi.n.mu")
+  # mcmcConf$addSampler(target = "phi.n.mu", type = "slice")
+  
+  mcmcBuild <- buildMCMC(mcmcConf)
+  compMCMC <- compileNimble(mcmcBuild)
+  compMCMC$run(niter = 500)
+  
+  library(coda)
+  
+  mcmc.eff <- tibble(
+    node = names(effectiveSize(as.matrix(compMCMC$mvSamples))),
+    effSize_default = effectiveSize(as.matrix(compMCMC$mvSamples)) %>% round(2),
+    accRate_default = 1 - rejectionRate(as.mcmc(as.matrix(compMCMC$mvSamples)) %>% round(2))
+  )
+  
+  print(as.matrix(mcmc.eff))
+  print(head(as.matrix(compMCMC$mvSamples)))
+  
+}
 
-model$initializeInfo()
-cModel <- compileNimble(model)
-monitor.check <- monitor[monitor != "x"]
-mcmcConf <- configureMCMC(cModel, monitors = monitor.check)
+if(n.slots > 2){
+  source("Functions/run_nimble_parallel2.R")
+  cl <- makeCluster(n.slots)
+  samples <- run_nimble_parallel(
+    cl = cl,
+    model = model.code,
+    constants = constants,
+    data = data,
+    inits = inits,
+    monitor = monitor,
+    n.iter = n.iter,
+    n.burnin = n.burnin,
+    n.ens = 10000,
+    check.interval = 5,
+    max.iter = max.iter,
+    calculate = TRUE,
+    file.name = file.path(site.dir, "NIMBLE_check.RData")
+  )
+  stopCluster(cl)
 
-# mcmcConf$removeSampler(target = "phi.a.mu")
-# mcmcConf$addSampler(target = "phi.a.mu", type = "slice")
-# mcmcConf$removeSampler(target = "phi.l.mu")
-# mcmcConf$addSampler(target = "phi.l.mu", type = "slice")
-# mcmcConf$removeSampler(target = "phi.n.mu")
-# mcmcConf$addSampler(target = "phi.n.mu", type = "slice")
 
-mcmcBuild <- buildMCMC(mcmcConf)
-compMCMC <- compileNimble(mcmcBuild)
-compMCMC$run(niter = 2500)
-
-library(coda)
-
-mcmc.eff <- tibble(
-  node = names(effectiveSize(as.matrix(compMCMC$mvSamples))),
-  effSize_default = effectiveSize(as.matrix(compMCMC$mvSamples)) %>% round(2),
-  accRate_default = 1 - rejectionRate(as.mcmc(as.matrix(compMCMC$mvSamples)) %>% round(2))
-)
-
-print(as.matrix(mcmc.eff))
-print(head(as.matrix(compMCMC$mvSamples)))
-
+  message("Writing output...")
+  save(samples,
+       file = file.path(site.dir, "NIMBLE_samples.RData"))
+  message("--- Done ---")
+}
